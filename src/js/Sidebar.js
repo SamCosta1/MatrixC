d3c1aefc09701f60bb45ed1b0fe51dd07acbf434
Sam <samuel.dacosta@student.manchester.ac.uk>
2016-08-06 16:26:53 +0100

    Style sidebar and fix resizing issues

--- src/js/Sidebar.js
+++ src/js/Sidebar.js
@@ -1,15 +1,31 @@
 $('.sidebar').ready(function() {
+    $('.sidebar').css('max-width', $(window).width());
     var oldX = 0;
     $('.handle').mousedown(function() {
         $(document).bind('mousemove', function(e) {
             e.stopImmediatePropagation();
             $('.sidebar').width($('.sidebar').width() + $('.sidebar').position().left - e.pageX);
+            if ($('.sidebar').width() !== 16)
+                $('.snapHandle').removeClass('expand');
+            else
+                $('.snapHandle').addClass('expand');
         });
     });
+
     $(document).mouseup(function(e) {
         $(document).unbind('mousemove');
     });
+
+    $('.snapHandle').click(function() {
+        if ($('.sidebar').width() == 16) {
+            $('.sidebar').width($(window).width() * 0.75);
+            $('.snapHandle').removeClass('expand')
+        } else {
+            $('.sidebar').width(16);
+            $('.snapHandle').addClass('expand');
+        }
+    });
 });
-$(window).resize(function () {
-    console.log("resize");
+$(window).resize(function() {
+    $('.sidebar').css('max-width', $(window).width());
 });
