10f9ce3d2d84f7641c24d0af465599be1dff3720
Sam <samuel.dacosta@student.manchester.ac.uk>
2016-08-30 11:36:56 +0100

    Add icon to collapse header on and create a new js file to handle the events

---
+++ src/js/Header.js
@@ -0,0 +1,33 @@
+function Header() {
+    var $mainHeader = $('#header'),
+        $title = $('.middleTitle'),
+        $menu = $('.headerMenu'),
+        $toggle = $('.headerToggleBar'),
+
+        collapsed = false;
+
+    function init() {
+        $toggle.click(onToggleClick);
+    }
+
+    function onToggleClick() {
+        collapsed = !collapsed;
+
+        if (collapsed) {
+            $mainHeader.addClass('collapsed');
+
+            $toggle.removeClass('icon-double-up-arrows');
+            $toggle.addClass('icon-double-down-arrows');
+        } else {
+            $mainHeader.removeClass('collapsed');
+            $toggle.addClass('icon-double-up-arrows');
+            $toggle.removeClass('icon-double-down-arrows');
+        }
+
+
+    }
+
+    return {
+        init: init
+    }
+}
